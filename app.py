9e432146e90635442536c64cf616f1b43a4bd037
meganmcadams <megmcmuffin14@gmail.com>
2023-08-02 11:33:11 -0500

    renaming routes to .html

--- app.py
+++ app.py
@@ -5,18 +5,19 @@ app = Flask(__name__)
 
 # PAGES
 @app.route('/')
+@app.route('/index.html')
 def page_index():    
     return render_template('index.html')
 
-@app.route('/projects')
+@app.route('/projects.html')
 def page_projects():
     return render_template('projects.html')
 
-@app.route('/awards')
+@app.route('/awards.html')
 def page_awards():
     return render_template('awards.html')
 
-@app.route('/professional-experience')
+@app.route('/professional-experience.html')
 def page_professional_experience():
     return render_template('professional_experience.html')
 
